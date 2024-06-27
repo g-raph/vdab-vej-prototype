e9911924b91d0a8153ef42bbf90fad032aa6452d
g-raph <gunter.gielen@telenet.be>
2024-06-27 16:22:51 +0200

    server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/vej-prototype'));
+app.use(express.static(__dirname + '/dist/vej-prototype/browser'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/vej-prototype/index.html'));
+res.sendFile(path.join(__dirname+'/dist/vej-prototype/browser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
