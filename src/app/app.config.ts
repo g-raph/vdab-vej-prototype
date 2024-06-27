061f62324f4cc12713579bc969a83425d79ff907
g-raph <gunter.gielen@telenet.be>
2024-06-27 13:18:10 +0200

    pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(
-    routes,
-    withInMemoryScrolling({
-      scrollPositionRestoration: "top",
-    })
-  )]
+  providers: [provideRouter(routes, withInMemoryScrolling({
+        scrollPositionRestoration: "top",
+    })), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
