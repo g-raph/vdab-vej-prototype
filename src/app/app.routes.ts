e321141691f8e917fa7d17c28e4e08ba01c2579d
g-raph <gunter.gielen@telenet.be>
2024-07-01 16:17:22 +0200

    remarks meeting 1/6/2024

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { JobListComponent } from './jobs/job-list/job-list.component';
 import { JobDetailComponent } from './jobs/job-detail/job-detail.component';
+import { JobSearchComponent } from './jobs/job-search/job-search.component';
 
 export const routes: Routes = [
+    {path:'zoek-een-job', component: JobSearchComponent},
     {path:'vind-een-job', component: JobListComponent},
     {path:'vind-een-job/:id', component: JobDetailComponent},
-    {path:'**', redirectTo:'vind-een-job'},
+    {path:'**', redirectTo:'zoek-een-job'},
 ];
---
+++ src/app/jobs/job-search/job-search.component.ts
@@ -0,0 +1,13 @@
+import { Component, ElementRef } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-job-search',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './job-search.component.html',
+  styleUrl: './job-search.component.scss'
+})
+export class JobSearchComponent {
+  
+}
