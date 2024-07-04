08c468197eac1fbd0850277f82555a47a9625f09
g-raph <gunter.gielen@telenet.be>
2024-07-04 15:35:01 +0200

    badge enzo

--- src/app/shell/footer/footer.component.ts
+++ src/app/shell/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { NgIf } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { map } from 'rxjs';
+import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, NgIf],
+  imports: [RouterLink, RouterLinkActive, NgIf, AsyncPipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
@@ -13,6 +15,10 @@ export class FooterComponent {
 
   @Input() backIcon = false;
 
-  constructor(){}
+  favorites$ = this.api.getJobs().pipe(
+    map(items => items.filter(item => item.saved))
+  );
+  constructor(private api: ApiService){
+  }
 
 }
