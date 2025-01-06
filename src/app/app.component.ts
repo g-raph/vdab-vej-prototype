4fdb7f1886b7ec73df10d692d15cc5fb6559ec50
g-raph <gunter.gielen@telenet.be>
2025-01-06 14:35:39 +0100

    bugfix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shell/header/header.component';
-import { FooterComponent } from './shell/footer/footer.component';
-import { CommonModule, NgForOf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { FilterModalService } from './filter-modal.service';
 import { ScrollTopComponent } from './shell/scroll-top/scroll-top.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HeaderComponent, FooterComponent, NgForOf, ScrollTopComponent],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, ScrollTopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
