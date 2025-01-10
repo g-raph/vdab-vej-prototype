a209d0039ee9c2ad24ca7d4c9f8579f1ffe19ebb
g-raph <gunter.gielen@telenet.be>
2025-01-10 15:53:38 +0100

    cta blocks on jobpage

--- src/app/jobs/job-search/job-search.component.ts
+++ src/app/jobs/job-search/job-search.component.ts
@@ -1,13 +1,30 @@
 import { Component, ElementRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CtaBlockComponent, ctaBlockItem } from '../../components/cta-block/cta-block.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-job-search',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CtaBlockComponent, NgFor],
   templateUrl: './job-search.component.html',
   styleUrl: './job-search.component.scss'
 })
 export class JobSearchComponent {
-  
+  ctaBlockItems: ctaBlockItem[] = [
+      {
+        id: 0,
+        title: 'Tips, voorbeelden en templates',
+        text: 'Ontdek sollicitatietips en handige checklists. Laat je inspireren door voorbeeldbrieven. Ga aan de slag met aantrekkelijke templates.',
+        link: '/vind-een-job',
+        label: 'Bekijk onze sollicitatie-info',
+      },
+      {
+        id: 1,
+        title: 'Hulp van onze sollicitatiecoaches',
+        text: 'Laat je sollicitatiebrief en cv nakijken. Oefen samen je sollicitatiegesprek. Stel al je vragen over het sollicitatieproces.',
+        link: '/vind-een-job',
+        label: 'Schakel onze online sollicitatiecoaches in',
+      }
+    ];
 }
