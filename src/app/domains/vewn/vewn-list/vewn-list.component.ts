81eeb568896ae60ad3ca5a0cc993e37cdbd9b20e
g-raph <gunter.gielen@telenet.be>
2025-09-03 14:15:45 +0200

    mobile header link

--- src/app/domains/vewn/vewn-list/vewn-list.component.ts
+++ src/app/domains/vewn/vewn-list/vewn-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-vewn-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './vewn-list.component.html',
   styleUrl: './vewn-list.component.scss'
 })
