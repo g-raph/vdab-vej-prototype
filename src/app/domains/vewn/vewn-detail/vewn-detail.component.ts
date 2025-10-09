c81fa80d26893055abcc02c91b8cbb1a528479c4
g-raph <gunter.gielen@telenet.be>
2025-10-09 10:06:12 +0200

    vewn

--- src/app/domains/vewn/vewn-detail/vewn-detail.component.ts
+++ src/app/domains/vewn/vewn-detail/vewn-detail.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-vewn-detail',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './vewn-detail.component.html',
   styleUrl: './vewn-detail.component.scss'
 })
